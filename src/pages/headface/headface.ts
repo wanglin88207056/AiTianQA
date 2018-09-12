e07636ee19f705a538b6f380ab4083d65593aee3
wanglin <88207056@qq.com>
2018-09-12 19:39:11 +0800

    获取图片方法引入相关服务

--- src/pages/headface/headface.ts
+++ src/pages/headface/headface.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import {
   ActionSheetController, IonicPage, LoadingController, ModalController, NavController, NavParams,
-  ToastController
+  ToastController, Platform
 } from 'ionic-angular';
 import {RestProvider} from "../../providers/rest/rest";
 import {Storage} from "@ionic/storage";
 import {BaseUI} from "../../common/baseui";
+
+// 导入外部加载进来的组件
+import { File } from "@ionic-native/file";
+import { FileTransfer,  FileTransferObject } from '@ionic-native/file-transfer';
+import {  FilePath } from "@ionic-native/file-path";
+import {  Camera } from "@ionic-native/camera";
 
 /**
  * Generated class for the HeadfacePage page.
@@ -23,6 +29,7 @@ export class HeadfacePage extends BaseUI{
 
   userId: string;
   errorMessage: any;
+  lastImage: string = null;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -31,7 +38,13 @@ export class HeadfacePage extends BaseUI{
               public loadingCtrl: LoadingController,
               public toastCtrl: ToastController,
               public rest: RestProvider,
-              public actionSheetCtrl: ActionSheetController) {
+              public actionSheetCtrl: ActionSheetController,
+              public camera: Camera,
+              public transfer: FileTransfer,
+              public file: File,
+              public filePath: FilePath,
+              public platform: Platform
+              ) {
     super();
   }
 
@@ -67,6 +80,24 @@ export class HeadfacePage extends BaseUI{
       ]
     });
     actionSheet.present();
+  }
+
+  //获取图片
+  takePicture(sourceTye){
+    // 定义相机的一些参数
+    var options = {
+      quality: 100,
+      sourceType: sourceTye,
+      saveToPhotoAlbum: false,//s是否保存照片到相册中
+      correctOrientation: true, // 是否纠正拍摄照片的方向
+    }
+    //获取图片的方法
+    this.camera.getPicture(options).then((imagePath) => {
+      //特别处理Android平台的文件路径问题
+      if(this.platform.is('android')){
+        
+      }
+    });
   }
 
 }
