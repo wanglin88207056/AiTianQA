1b85566cc7a17f6e5c32a4d39324b085864b366e
wanglin <88207056@qq.com>
2018-09-17 18:27:23 +0800

    获取图片并处理

--- src/pages/headface/headface.ts
+++ src/pages/headface/headface.ts
@@ -7,6 +7,7 @@ import {RestProvider} from "../../providers/rest/rest";
 import {Storage} from "@ionic/storage";
 import {BaseUI} from "../../common/baseui";
 
+declare  var cordova: any; //导入第三方的库到ts项目中
 // 导入外部加载进来的组件
 import { File } from "@ionic-native/file";
 import { FileTransfer,  FileTransferObject } from '@ionic-native/file-transfer';
@@ -94,10 +95,44 @@ export class HeadfacePage extends BaseUI{
     //获取图片的方法
     this.camera.getPicture(options).then((imagePath) => {
       //特别处理Android平台的文件路径问题
-      if(this.platform.is('android')){
-        
+      if(this.platform.is('android') && sourceTye == this.camera.PictureSourceType.PHOTOLIBRARY){
+        this.filePath.resolveNativePath(imagePath) //获取android平台下的真实路径
+          .then(filePath =>{
+            //获取正确的路径
+            let correctPath = filePath.substr(0,filePath.lastIndexOf('/')+1);
+            //获取正确文件名
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/')+1,imagePath.lastIndexOf('?')+1);
+            this.copyFileToLocalDir(correctPath,currentName,this.createFileName());
+          });
+      }else{
+        //获取正确的路径
+        let correctPath = imagePath.substr(0,imagePath.lastIndexOf('/') + 1);
+        //获取正确文件名
+        let currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath,currentName,this.createFileName());
       }
+    },(err) => {
+        super.showToast(this.toastCtrl,"选择图片出现错误,请在 App 中操作或检查相关权限。")
     });
   }
 
+  //将获取到的图片或者相机拍摄到的图片进行一下另存为,用于后期图片上传使用
+  copyFileToLocalDir(namePath,currentName,newFileName){
+    this.file.copyFile(namePath,currentName,cordova.file.dataDirectory,newFileName)
+      .then(
+        success => {
+          this.lastImage = newFileName;
+        },error => {
+          super.showToast(this.toastCtrl,"存储图片到本地图库出现错误.");
+        }
+      )
+  }
+
+  //为文件生成一个新的文件名
+  createFileName(){
+    var d = new Date(),
+      n = d.getTime(),
+      newFileName = n+".jpg";
+    return newFileName;
+  }
 }
